059385f5c29d9f76552648c5a9c7c15dc2285009
Yaroslav Spirin <yaroslav.spirin@webpros.com>
2025-11-11 17:18:43 +0200

    bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,12 @@ set -e
 echo "🔍 Проверяем файлы на наличие копирайта..."
 echo "Строка для поиска: $COPYRIGHT_LINE"
 
-YEAR=$(date -u +"%Y")
-COPYRIGHT_LINE="Copyright 2013-$YEAR."
-
 echo "Изменённые файлы:"
 echo "$CHANGED_FILES"
 
 SUPPORTED_EXTENSIONS="bat|c|cc|cd|cjs|config|cpp|cs|css|dist|go|h|hpp|htm|html|in|ini|js|l|less|map|mjs|php|phtml|pl|pm|py|sh|sj|sql|tcc|tpl|ts|tsx|vbs|wxi|wxs|xml|xsd|xsl|xslt|java|yaml|toml|yml"
 
-FILES=$(printf "%s\n" "$CHANGED_FILES" | tr ' ' '\n' |
+FILES=$(printf "%s\n" "$CHANGED_FILES" |
   grep -E "\.($SUPPORTED_EXTENSIONS)$" |
   (while read -r file; do [ -f "$file" ] && echo "$file"; done || true) |
   xargs -r grep -L "$COPYRIGHT_LINE" |
