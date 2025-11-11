2b09b515d51113b5ac10ec0bdda59ce7f6d5fd76
Yaroslav Spirin <yaroslav.spirin@webpros.com>
2025-11-11 16:05:04 +0200

    bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-FILES=$(find . \
-  -path "./.git" -prune -o \
-  -type f -print)
-echo "$FILES"
+#FILES=$(find . \
+#  -path "./.git" -prune -o \
+#  -type f -print)
+#echo "$FILES"
+
+CHANGED_FILES=$(git diff --name-only origin/${GITHUB_BASE_REF}...origin/${GITHUB_HEAD_REF})
+ecgo "$CHANGED_FILES"
