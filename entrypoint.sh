47805f982085f3f510a306f593d651c545356169
Yaroslav Spirin <yaroslav.spirin@webpros.com>
2025-11-11 16:18:24 +0200

    bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,23 @@ set -e
 
 CHANGED_FILES=$1
 
+YEAR=$(date -u +"%Y")
+COPYRIGHT_LINE="Copyright 2013-$YEAR."
+
 echo "Изменённые файлы:"
 echo "$CHANGED_FILES"
+
+SUPPORTED_EXTENSIONS="bat|c|cc|cd|cjs|config|cpp|cs|css|dist|go|h|hpp|htm|html|in|ini|js|l|less|map|mjs|php|phtml|pl|pm|py|sh|sj|sql|tcc|tpl|ts|tsx|vbs|wxi|wxs|xml|xsd|xsl|xslt|java|yaml|toml|yml"
+
+FILES=$(printf "%s\n" "$CHANGED_FILES" |
+  grep -E "\.($SUPPORTED_EXTENSIONS)$" |
+  (while read -r file; do [ -f "$file" ] && echo "$file"; done || true) |
+  xargs -r grep -L "$COPYRIGHT_LINE" |
+  cat )
+
+if [[ -n $FILES ]]; then
+  echo -e "You must add \"$COPYRIGHT_LINE\" to the following files: \n$FILES"
+  exit 2
+fi
+
+exit 0
